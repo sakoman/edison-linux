872936f44da1925332ed6cfe1b4a618dbb60f7f1
Steve Sakoman <steve@sakoman.com>
2017-06-20 12:46:16 -1000

    add tca6507 support to board file

--- arch/x86/platform/intel-mid/board.c
+++ arch/x86/platform/intel-mid/board.c
@@ -78,6 +78,7 @@
 
 #include "device_libs/platform_wm8994.h"
 #include "device_libs/platform_ds1307.h"
+#include "device_libs/platform_tca6507.h"
 
 /*
  * SPI devices
@@ -126,6 +127,7 @@ struct devs_id __initconst device_ids[] = {
 	{"mpu3050", SFI_DEV_TYPE_I2C, 1, &mpu3050_platform_data, NULL},
 	{"i2c_disp_brig", SFI_DEV_TYPE_I2C, 0, &tc35876x_platform_data, NULL},
 	{"ds3231", SFI_DEV_TYPE_I2C, 0, &ds1307_platform_data, NULL},
+	{"tca6507", SFI_DEV_TYPE_I2C, 0, &tca6507_platform_data, NULL},
 
 	/* MSIC subdevices */
 	{"msic_adc", SFI_DEV_TYPE_IPC, 1, &msic_adc_platform_data,
